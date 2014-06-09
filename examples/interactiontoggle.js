1b1202ac838726404c82ba0ccf4314b7463929c1
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-09 15:19:08 +0200

    Add an interaction toggle example

---
+++ examples/interactiontoggle.js
@@ -0,0 +1,46 @@
+goog.provide('interactiontoggle');
+
+goog.require('go_interactiontoggle_directive');
+goog.require('go_map_directive');
+goog.require('ol.FeatureOverlay');
+goog.require('ol.Map');
+goog.require('ol.View2D');
+goog.require('ol.interaction.Draw');
+goog.require('ol.layer.Tile');
+goog.require('ol.source.MapQuest');
+
+(function() {
+  var module = angular.module('app', ['go']);
+
+  module.controller('MainController', ['$scope',
+    /**
+     * @param {angular.Scope} $scope Scope.
+     */
+    function($scope) {
+
+      /** @type {ol.Map} */
+      $scope.map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.MapQuest({layer: 'sat'})
+          })
+        ],
+        view: new ol.View2D({
+          center: [-10997148, 4569099],
+          zoom: 4
+        })
+      });
+
+      var featureOverlay = new ol.FeatureOverlay();
+      featureOverlay.setMap($scope.map);
+
+      /** @type {ol.interaction.Draw} */
+      $scope.interaction = new ol.interaction.Draw(
+          /** @type {olx.interaction.DrawOptions} */ ({
+            type: 'Point',
+            features: featureOverlay.getFeatures()
+          }));
+
+    }]);
+})();
+
