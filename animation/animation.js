88481e69bc3c756110b5969a6db0802dabe6e8e8
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-19 12:35:00 +0100

    Update GitHub pages

--- animation/animation.js
+++ animation/animation.js
@@ -10,7 +10,7 @@
    * controller has a property "map" including a reference to the OpenLayers
    * map.
    */
-  module.directive('appMap', ['$animate', '$window',
+  module.directive('appMap', [
     /**
      * @return {angular.Directive} The directive specs.
      */
@@ -21,49 +21,12 @@
           'map': '=appMap',
           'class': '=appMapClass'
         },
-        controller: 'AppMapController',
+        controller: function() {},
         controllerAs: 'ctrl',
         bindToController: true,
-        template: '<div ngeo-map="ctrl.map"></div>'
+        template: '<div ngeo-map="ctrl.map" ngeo-resizemap="ctrl.map" ' +
+            'ngeo-resizemap-class="ctrl.class"></div>'
       };
-    }]);
-
-
-  /**
-   * Controller for the appMap directive. The controller is responsible for
-   * adding/removing the animation class to/from the map element. It runs
-   * a requestAnimationFrame loop to update the map size during the animation.
-   */
-  module.controller('AppMapController', [
-    '$scope', '$element', '$animate', '$window',
-    /**
-     * @param {angular.Scope} $scope Scope.
-     * @param {angular.JQLite} $element Element.
-     * @param {angular.$animate} $animate Angular animate service.
-     * @param {angular.$window} $window Angular window service.
-     */
-    function($scope, $element, $animate, $window) {
-      var map = this['map'];
-      $scope.$watch(angular.bind(this, function() {
-        return this['class'];
-      }), function(newVal, oldVal) {
-        if (newVal != oldVal) {
-          var stop = false;
-          var mapElement = $element.find('div');
-          var promise = $animate.setClass(mapElement, newVal, oldVal);
-          $window.requestAnimationFrame(function cb() {
-            if (!stop) {
-              map.updateSize();
-              $window.requestAnimationFrame(cb);
-            }
-          });
-          // the promise is resolved when the animation completes
-          promise.then(function() {
-            map.updateSize();
-            stop = true;
-          });
-        }
-      });
     }]);
 
 
