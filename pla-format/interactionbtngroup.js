21ebe39d51711394717a7660b9854d726a750fcc
Éric Lemoine <eric.lemoine@gmail.com>
2015-08-28 15:09:06 +0200

    Update GitHub pages

---
+++ pla-format/interactionbtngroup.js
@@ -0,0 +1,109 @@
+
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @param {ngeo.DecorateInteraction} ngeoDecorateInteraction Decorate
+ *     interaction service.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function(ngeoDecorateInteraction) {
+  var source = new ol.source.Vector();
+
+  var vector = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#ffcc33',
+        width: 2
+      }),
+      image: new ol.style.Circle({
+        radius: 7,
+        fill: new ol.style.Fill({
+          color: '#ffcc33'
+        })
+      })
+    })
+  });
+
+  /**
+   * @type {ol.Map}
+   * @export
+   */
+  this.map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.MapQuest({layer: 'sat'})
+      }),
+      vector
+    ],
+    view: new ol.View({
+      center: [-10997148, 4569099],
+      zoom: 4
+    })
+  });
+
+  var map = this.map;
+
+  /**
+   * @type {ol.interaction.Draw}
+   * @export
+   */
+  this.drawPolygon = new ol.interaction.Draw(
+      /** @type {olx.interaction.DrawOptions} */ ({
+        type: 'Polygon',
+        source: source
+      }));
+
+  var drawPolygon = this.drawPolygon;
+
+  drawPolygon.setActive(false);
+  ngeoDecorateInteraction(drawPolygon);
+  map.addInteraction(drawPolygon);
+
+  /**
+   * @type {ol.interaction.Draw}
+   * @export
+   */
+  this.drawPoint = new ol.interaction.Draw(
+      /** @type {olx.interaction.DrawOptions} */ ({
+        type: 'Point',
+        source: source
+      }));
+
+  var drawPoint = this.drawPoint;
+  drawPoint.setActive(false);
+  ngeoDecorateInteraction(drawPoint);
+  map.addInteraction(drawPoint);
+
+  /**
+   * @type {ol.interaction.Draw}
+   * @export
+   */
+  this.drawLine = new ol.interaction.Draw(
+      /** @type {olx.interaction.DrawOptions} */ ({
+        type: 'LineString',
+        source: source
+      }));
+
+  var drawLine = this.drawLine;
+  drawLine.setActive(false);
+  ngeoDecorateInteraction(drawLine);
+  map.addInteraction(drawLine);
+
+};
+
+
+app.module.controller('MainController', app.MainController);
